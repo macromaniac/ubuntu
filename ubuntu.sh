846e963bb3eb98b54e563efae67930b1eced748f
Joey Caero <joeycaero@gmail.com>
2016-09-14 05:47:12 -0500

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -23,7 +23,10 @@ apt -y install python-numpy python-scipy python-matplotlib ipython ipython-noteb
 apt -y install pkg-config
 apt -y install libpng-dev libjpeg8-dev libfreetype6-dev
 pip install matplotlib
+pip install keras
 pip install scikit-learn
+pip install --no-use-wheel --upgrade distribute
+pip installscikit-image
 
 #we use cpu tf atm, from https://www.tensorflow.org/versions/r0.10/get_started/os_setup.html
 export TF_BINARY_URL=https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.10.0rc0-cp27-none-linux_x86_64.whl
